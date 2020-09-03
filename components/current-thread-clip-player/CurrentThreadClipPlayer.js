1738050776883fa85756ffb257180d5579e83ca5
Chris Co <ufcordle@hotmail.com>
2020-09-03 03:35:31 -0500

    Customizing overlays and wave styles

--- components/current-thread-clip-player/CurrentThreadClipPlayer.js
+++ components/current-thread-clip-player/CurrentThreadClipPlayer.js
@@ -1,90 +1,111 @@
 import React, { useEffect, useState } from 'react';
-import { Box, Flex, Stack, useColorMode, Grid, Text } from '@chakra-ui/core';
-import AudioMotionAnalyzer from 'audiomotion-analyzer';
+import {
+	Box,
+	Flex,
+	Stack,
+	useColorMode,
+	Grid,
+	Text,
+	Button,
+	Slider,
+	SliderTrack,
+	SliderFilledTrack,
+	SliderThumb,
+} from '@chakra-ui/core';
 import videojs from 'video.js';
 import WaveSurfer from 'wavesurfer.js';
 import Wavesurfer from 'videojs-wavesurfer/dist/videojs.wavesurfer.js';
 
-// https://file-examples-com.github.io/uploads/2017/11/file_example_MP3_700KB.mp3
+import styles from './CurrentThreadClipPlayer.module.css';
+import { Avatar } from '../avatar';
 
 export default function CurrentThreadClipPlayer() {
 	let options = {
 		controls: true,
-		bigPlayButton: true,
 		autoplay: false,
 		loop: false,
-		fluid: false,
-		width: 600,
-		height: 300,
+		fluid: true,
+		aspectRatio: '16:9',
+		controlBar: {
+			fullscreenToggle: false,
+		},
 		plugins: {
-			// enable videojs-wavesurfer plugin
 			wavesurfer: {
-				// configure videojs-wavesurfer
 				backend: 'MediaElement',
-				displayMilliseconds: true,
+				displayMilliseconds: false,
 				debug: true,
-				waveColor: 'grey',
-				progressColor: 'black',
+				waveColor: 'orange',
+				progressColor: 'brown',
 				cursorColor: 'black',
 				hideScrollbar: true,
+				barWidth: 2,
 			},
 		},
 	};
 
 	const [ player, setPlayer ] = useState(null);
+	const [ timer, setTimer ] = useState(0);
 
-	useEffect(
-		() => {
-			if (!player) {
-				const audioNode = document.getElementById('my-audio');
-				// const audioNode = new Audio(
-				// 	'https://file-examples-com.github.io/uploads/2017/11/file_example_MP3_700KB.mp3'
-				// );
+	useEffect(() => {
+		if (!player) {
+			const audioNode = document.createElement('audio');
+			audioNode.classList.add('video-js', 'vjs-default-skin', styles.audioNodeBackground);
+			const containerNode = document.getElementsByClassName(styles.audioContainer)[0];
+			containerNode.appendChild(audioNode);
 
-				console.log({ audioNode });
-
-				console.log({ Wavesurfer });
-
-				let newPlayer = videojs(audioNode, options, () => {
-					// print version information at startup
-					// var version_info =
-					// 	'Using video.js ' +
-					// 	videojs.VERSION +
-					// 	' with videojs-wavesurfer ' +
-					// 	videojs.getPluginVersion('wavesurfer') +
-					// 	' and wavesurfer.js ' +
-					// 	WaveSurfer.VERSION;
-					// videojs.log(version_info);
-
-					// load file
-					newPlayer.src({
-						src: 'https://file-examples-com.github.io/uploads/2017/11/file_example_MP3_700KB.mp3',
-						type: 'audio/mpeg',
-					});
+			let newPlayer = videojs(audioNode, options, () => {
+				newPlayer.src({
+					src: 'https://file-examples-com.github.io/uploads/2017/11/file_example_MP3_700KB.mp3',
+					type: 'audio/mpeg',
 				});
+			});
 
-				newPlayer.on('waveReady', (event) => {
-					console.log('waveform: ready!');
-				});
+			newPlayer.on('waveReady', (event) => {
+				console.log('waveform: ready!');
+			});
 
-				newPlayer.on('playbackFinish', (event) => {
-					console.log('playback finished');
-				});
+			newPlayer.on('playbackFinish', (event) => {
+				console.log('playback finished');
+			});
 
-				setPlayer(newPlayer);
+			newPlayer.on('timeupdate', (event) => {
+				setTimer(newPlayer.currentTime());
+			});
+
+			setPlayer(newPlayer);
+		}
+
+		return () => {
+			if (player) {
+				player.dispose();
 			}
 
-			//TODO: Set up will unmount equivalent
-		},
-		[ WaveSurfer, Wavesurfer ]
-	);
+			setPlayer(null);
+		};
+	}, []);
 
 	return (
 		<Box>
 			<Text>CurrentThreadClipPlayer</Text>
-			<div data-vjs-player>
-				<audio id="my-audio" className="video-js vjs-default-skin" />
-			</div>
+			<div>{timer}</div>
+			<Slider
+				color="pink"
+				defaultValue={0}
+				value={player ? timer / player.duration() * 100 : 0}
+				onChange={(val) => player.currentTime(val / 100 * player.duration())}
+			>
+				<SliderTrack />
+				<SliderFilledTrack />
+				<SliderThumb />
+			</Slider>
+			<Box className={styles.audioContainer} bg="green.500" w={420}>
+				<Stack className={styles.avatarContainer} h="100%" w="100%" alignItems="center" justifyContent="center">
+					<Box h={20} w={20} bg="blue.500" borderRadius={5}>
+						<Avatar />
+					</Box>
+					<Text>Christopher Johnson</Text>
+				</Stack>
+			</Box>
 		</Box>
 	);
 }
