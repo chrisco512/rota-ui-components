65a42af692034d0ee3a21c31af8799f4ff49f2e0
Chris Co <ufcordle@hotmail.com>
2020-12-06 13:53:34 -0600

    Removing audiomotion transpile config

--- next.config.js
+++ next.config.js
@@ -1,3 +1,3 @@
-const withTM = require('next-transpile-modules')([ 'react-syntax-highlighter', 'audiomotion-analyzer' ]); // pass the modules you would like to see transpiled
+const withTM = require('next-transpile-modules')([ 'react-syntax-highlighter' ]); // pass the modules you would like to see transpiled
 
 module.exports = withTM();
