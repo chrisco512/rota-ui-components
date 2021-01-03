815bee3a2166927f75411af785774e8920fad579
Chris Co <ufcordle@hotmail.com>
2021-01-02 23:36:57 -0600

    Fixing a bug

--- components/room-view/RoomTableItemMembers.js
+++ components/room-view/RoomTableItemMembers.js
@@ -6,13 +6,13 @@ const MAX_TABLE_ITEM_MEMBERS = 6;
 
 function RoomTableItemMembers({ members = [] }) {
 	const membersToDisplay = members.slice(0, MAX_TABLE_ITEM_MEMBERS);
-	const emptySeats = new Array(MAX_TABLE_ITEM_MEMBERS - membersToDisplay.length);
+	const emptySeats = new Array(MAX_TABLE_ITEM_MEMBERS - membersToDisplay.length).fill('');
 
 	return (
 		<Box mr={4}>
 			<Grid templateColumns="repeat(3, 1fr)" gridGap={1}>
-				{membersToDisplay.map((member) => <RoomTableItemMemberAvatar avatar={member.avatar} />)}
-				{emptySeats.fill(<RoomTableItemMemberEmpty />)}
+				{membersToDisplay.map((member) => <RoomTableItemMemberAvatar avatar={member.avatar} key={member.id} />)}
+				{emptySeats.map((s, index) => <RoomTableItemMemberEmpty key={index} />)}
 			</Grid>
 		</Box>
 	);
